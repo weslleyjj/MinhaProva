b67f71acd2b06f308858f3bf290077d50e9d2d23
Jodeilson Weslley <jodeilson@ufrn.edu.br>
2021-03-13 13:30:28 -0300

    questão 3 e 4 OK

--- app/src/main/java/com/pdm/minhaprova/MainActivity.kt
+++ app/src/main/java/com/pdm/minhaprova/MainActivity.kt
@@ -3,6 +3,8 @@ package com.pdm.minhaprova
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        mensagemBemvindo()
 
         binding.button1.setOnClickListener {
 
@@ -39,6 +43,39 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(binding.root, "Cancelado", Snackbar.LENGTH_SHORT)
             snackbar.show()
         }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        println("Salvando os dados")
+        println(binding.text1.text.toString())
+        outState.putString("textView1", binding.text1.text.toString())
+        outState.putString("textView2", binding.text2.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.text1.text = savedInstanceState.getString("textView1")
+        binding.text2.text = savedInstanceState.getString("textView2")
+
+    }
+
+    fun mensagemBemvindo(){
+
+        val settings = getSharedPreferences("Main", MODE_PRIVATE)
+        val editor = settings.edit()
+
+        if(settings.getBoolean("primeiroAcesso", true)) {
+            val toast = Toast.makeText(applicationContext, "Bem-vindo!", Toast.LENGTH_LONG)
+            toast.show()
+
+            editor.putBoolean("primeiroAcesso", false)
+            editor.apply()
+        }
+
 
     }
 }
